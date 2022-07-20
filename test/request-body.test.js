03ad1b99221093cd53ddeb0d86f7a4c1478d161e
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-07-21 00:07:34 +0700

    :fist_left: Upload File

--- test/request-body.test.js
+++ test/request-body.test.js
@@ -1,9 +1,11 @@
 import express from "express";
 import request from "supertest";
+import expressFileUpload from "express-fileupload";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(expressFileUpload());
 
 app.post('/json', (req, res) => {
     const name = req.body.name;
@@ -17,6 +19,23 @@ app.post('/form', (req, res) => {
     res.json({
         hello: `Hello ${name}`
     });
+});
+
+app.post("/file", async (req, res) => {
+    const textFile = req.files.article;
+    await textFile.mv(__dirname + "/upload/" + textFile.name);
+
+    res.send(`Hello ${req.body.name}, you uploaded ${textFile.name}`);
+});
+
+test("Test Request File Upload", async () => {
+    const response = await request(app)
+        .post("/file")
+        .set("Content-Type", "multipart/form-data")
+        .field("name", "Eko")
+        .attach("article", __dirname + "/contoh.txt");
+
+    expect(response.text).toBe("Hello Eko, you uploaded contoh.txt");
 });
 
 test("Test Request JSON", async () => {
